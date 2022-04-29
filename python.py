961dd8f46d96aaafec3339fe49ee6b24bdb6358f
abduaziz <104622399+abduazizpoytug@users.noreply.github.com>
2022-04-29 14:12:06 +0500

    Add files via upload

---
+++ python.py
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+
+
+
+print("salom")
+value = input('')
+if value == "salom":
+    print("qales😉")
+elif value == "qales":
+    print("zor👌")
+
+
+
+
+
+
+
+
+
+
+# num = int(input('sonni kiriting'))
+# num2 = int(input('ikkinchi sonni kiriting'))
+# value = input("amalni kiriting: ")
+# if value == "+":
+#     print("natija: " + str(num + num2))
+# elif value =="-":
+#     print("natija: " + str(num - num2))
+# elif value =="*":
+#     print("natija: " + str(num * num2))
+# elif value =="/":
+#     print("natija: " + str(num / num2))
+
+
+
+
+
+
+
+
+
+# a = int(input("sonni kiriting: "))
+# b = int(input("ikkinchi sonni kiriting: "))
+# result = a + b
+# print("natija: " + str(result))
